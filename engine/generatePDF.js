5b528e126c41088fa6dac1ce95a98084e0f5f158
sudheeesh <sudheeshvilla5@gmail.com>
2026-03-09 22:22:48 +0530

    pupperter

--- engine/generatePDF.js
+++ engine/generatePDF.js
@@ -45,7 +45,18 @@ async function generatePDF(data) {
     .replace("{{rows}}", rows);
 
   const browser = await puppeteer.launch({
-    headless: "new"
+    headless: "new",
+    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || null,
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--disable-accelerated-2d-canvas",
+      "--no-first-run",
+      "--no-zygote",
+      "--single-process",
+      "--disable-gpu"
+    ],
   });
 
   const page = await browser.newPage();
